70e1c56386d0c3ea620e3cca24f5fd9a4b65f81d
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-30 20:28:03 +0530

    Second commit

--- script.js
+++ script.js
@@ -8,3 +8,11 @@
 // let ans = myFun();
 // console.log(ans);
 
+// 2) Fucntion Expression?
+
+// const myFun = function () {
+//   console.log("This is function Expression!");
+// }
+// console.log(myFun());
+
+// 3) What is the first class function means?

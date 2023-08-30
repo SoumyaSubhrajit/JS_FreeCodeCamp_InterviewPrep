5417467d59aa4fbc58f4c823cb9f0ce8684ae416
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-30 20:36:29 +0530

    First class variables.

--- script.js
+++ script.js
@@ -16,3 +16,31 @@
 // console.log(myFun());
 
 // 3) What is the first class function means?
+// A-> In JS function are treated like a variable..
+
+// let myFun = (num) => {
+//   return num * num
+// };
+
+// let ans = myFun(4);
+// console.log(ans);
+
+
+// By help of the cloures..
+// let myFun = (num) => {
+//   let a = num;
+//   return function (a) {
+//     console.log(num * num);
+//   }
+// };
+
+
+// let myFun = (func) => {
+//   console.log("This is innner function");
+//   func()
+// };
+
+// function myFun2() {
+//   console.log("This is outer  function");
+// }
+// myFun(myFun2);

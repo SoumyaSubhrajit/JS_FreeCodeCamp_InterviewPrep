268cd395e82fa53a89992dc6fbbe1bffb3c2720d
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-30 20:08:34 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// -- Function in JS.
+// 1) What is function Declaration?
+
+// function myFun() {
+//   console.log("Welcome to my Function.");
+//   return 6 * 6;
+// }
+// let ans = myFun();
+// console.log(ans);
+
